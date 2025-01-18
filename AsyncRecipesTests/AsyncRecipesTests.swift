fcc802957656158626645366f53e93f0c75ff4bc
Lucas Assis Rodrigues <lucas.assis.ro@gmail.com>
2025-01-17 19:03:49 -0500

    Initial Commit

---
+++ AsyncRecipesTests/AsyncRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncRecipesTests.swift
+//  AsyncRecipesTests
+//
+//  Created by Lucas Assis Rodrigues on 1/17/25.
+//
+
+import Testing
+@testable import AsyncRecipes
+
+struct AsyncRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
