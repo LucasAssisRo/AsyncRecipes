434e693b281fcb21658e220c3e3146bae7dff895
Lucas Assis Rodrigues <lucas.assis.ro@gmail.com>
2025-01-18 01:11:41 -0500

    chore: resolve warnings

--- AsyncRecipes/URL+.swift
+++ AsyncRecipes/URL+.swift
@@ -10,9 +10,9 @@ import Foundation
 extension URL {
     var asYoutubeEmbeded: Self? {
         guard case let youtube = "youtube",
-            absoluteString.contains("youtube"),
+            absoluteString.contains(youtube),
             case let watch = "watch?v=",
-            absoluteString.contains("watch?v=")
+            absoluteString.contains(watch)
         else { return nil }
         let embed = "embed/"
         return URL(string: absoluteString.replacingOccurrences(of: watch, with: embed))
